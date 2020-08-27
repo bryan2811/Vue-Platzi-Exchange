ba100af315529b8ff05781e2916431bc1503b572
Bryan <brandonb2816@gmail.com>
2020-08-27 12:35:33 -0300

    Mejorando la UI con filtros

--- src/main.js
+++ src/main.js
@@ -3,7 +3,11 @@ import App from './App.vue' // Importa el componente App
 import '@/assets/css/tailwind.css' // Importa Tailwind
 import '@/router'
 import router from '@/router'
+import { dollarFilter } from '@/filters'
+import { percentFilter } from '@/filters'
 
+Vue.filter('dollar', dollarFilter)
+Vue.filter('percent', percentFilter)
 Vue.config.productionTip = false
 
 new Vue({
