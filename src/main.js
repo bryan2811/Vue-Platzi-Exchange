b4e2b3ae09361c92e0a20a88b970875418560c59
Bryan <brandonb2816@gmail.com>
2020-08-27 10:51:04 -0300

    Agregando el plugin de TailwindCSS

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from "vue"; // Importa la librería desde NPM
 import App from "./App.vue"; // Importa el componente App
+import "@/assets/css/tailwind.css" // Importa Tailwind
 
 Vue.config.productionTip = false;
 
