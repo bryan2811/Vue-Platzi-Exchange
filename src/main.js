28cd51993000ca33b6397fe48676b32f00cfbfcf
Bryan <brandonb2816@gmail.com>
2020-08-27 10:31:20 -0300

    Initial Commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
-import Vue from "vue";
-import App from "./App.vue";
+import Vue from "vue"; // Importa la librería desde NPM
+import App from "./App.vue"; // Importa el componente App
 
 Vue.config.productionTip = false;
 
-new Vue({
+new Vue({ //Instancia de Vue
   render: h => h(App)
 }).$mount("#app");
