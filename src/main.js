0558404f1e4d615a4bf8a55ed5dc60c43d5dd57a
Bryan <brandonb2816@gmail.com>
2020-08-27 11:48:48 -0300

    Introducción y setup de Vue Router

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import Vue from "vue"; // Importa la librería desde NPM
 import App from "./App.vue"; // Importa el componente App
-import "@/assets/css/tailwind.css" // Importa Tailwind
+import "@/assets/css/tailwind.css"; // Importa Tailwind
+import "@/router";
+import router from "@/router";
 
 Vue.config.productionTip = false;
 
 new Vue({
   //Instancia de Vue
-  render: h => h(App)
+  router: router,
+  render: (h) => h(App),
 }).$mount("#app");
